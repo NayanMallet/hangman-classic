b820b9040af8afdf1aa5950ba7444b50e31f5866
Pablo JL~ <pablo.jeanlouis@ynov.com>
2022-10-31 14:02:38 +0100

    adding comments to get-input and save_game

--- g-func/save_game.go
+++ g-func/save_game.go
@@ -15,17 +15,18 @@ type NewSave struct {
 }
 
 func SaveGame(save *NewSave) {
+	// Function who create a save of the game in a file "save.txt"
 	b, err := json.Marshal(save)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
 	file, err2 := os.Create("save.txt")
-	defer file.Close() // on ferme automatiquement à la fin de notre programme
+	defer file.Close() // Close the file at the end of the program
 	if err2 != nil {
 		fmt.Println("error:", err2)
 	}
 
-	_, err3 := file.WriteString(string(b)) // écrire dans le fichier
+	_, err3 := file.WriteString(string(b)) // Write in the file
 	if err3 != nil {
 		fmt.Println("error:", err3)
 	}
